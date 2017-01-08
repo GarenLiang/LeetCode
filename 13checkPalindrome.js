19e9d417216d0c90f4f750dfe43d6f918e14c846
GarenLiang <ideabreeze@gmail.com>
2017-01-07 17:25:50 -0800

    Create 13checkPalindrome.js

---
+++ 13checkPalindrome.js
@@ -0,0 +1,11 @@
+function isPalindrome(str){
+  var i,len=str.length;
+  for(i=0;i<len/2;i++){
+    if(str[i]!==str[len-i-1])
+      return false;
+  }
+  return true;
+}
+
+isPalindrome('dad')
+isPalindrome('tomato')
